061ab725d1054f206d4d5bdde9a79ce19c6ece65
PastPerson <serty1098@gmail.com>
2022-11-10 23:57:28 +0900

    add database

--- gogakproject/src/gogakproject/MyDatabase.java
+++ gogakproject/src/gogakproject/MyDatabase.java
@@ -2,13 +2,17 @@ package gogakproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MyDatabase {
+	private String url = "jdbc:mysql://localhost/airdatabase?characterEncoding=UTF-8&serverTimezone=UTC";
+	private String sql = "SELECT * FROM airportdata";
+	private Connection con = null;
+	
 	public void connectMyDatabase() {
-		String url = "jdbc:mysql://localhost/airdatabase?characterEncoding=UTF-8&serverTimezone=UTC";
 		
-		Connection con = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("데이터베이스에 연결 중");
@@ -20,7 +24,21 @@ public class MyDatabase {
 			System.out.println("SQLException: "+ex.getMessage());
 		}
 	}
+	public void printData() throws SQLException{
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			System.out.println(rs.getString(1) + "\t");
+			
+		}
+	}
 	public MyDatabase() {
 		connectMyDatabase();
+		try {
+			printData();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
