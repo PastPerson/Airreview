b8ea415c2988b6ddae61aa64d660a5beae55b0e4
logan1106 <114173553+logan1106@users.noreply.github.com>
2022-11-25 13:19:10 +0900

    Update Aireview.java

--- gogakproject/src/gogakproject/Aireview.java
+++ gogakproject/src/gogakproject/Aireview.java
@@ -1,9 +1,7 @@
 package gogakproject;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Aireview extends JFrame {
@@ -11,7 +9,7 @@ public class Aireview extends JFrame {
 	private MainFrame mainframe = null;
 	private ViewFrame viewframe = null;
 	private MyActionListener myac = null;
-	private static MyDatabase mydb = null;
+	
 	private int windowMode;
 	
 	public void change() {
@@ -20,38 +18,36 @@ public class Aireview extends JFrame {
 			getContentPane().add(viewframe);
 			revalidate();
 			repaint();
+			setLocationRelativeTo(null);
 			this.windowMode = 1;
 		}else if(this.windowMode == 1){
 			getContentPane().removeAll();
 			getContentPane().add(mainframe);
 			revalidate();
 			repaint();
+			setLocationRelativeTo(null);
 			this.windowMode = 0;
 		}
 	}
 	
-	public MyDatabase getdb() {
-		return mydb;
-	}
-	
 	public Aireview() {
-		super("에어리뷰");
-		try {
-			mydb = new MyDatabase();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		super("Air-Review");
+//		System.out.println(this.getSize());
 		this.setSize(1200,800);
 		this.myac = new MyActionListener(this);
 		this.mainframe = new MainFrame(this,myac);
 		this.viewframe = new ViewFrame(this,myac);
 		this.windowMode = 0;
+		setLocationRelativeTo(null);
+		
 		this.add(this.mainframe);
 	}
 	
 	public static void main(String[] args) {
 		JFrame fr = new Aireview();
+		
 		fr.setVisible(true);
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		new MyDatabase();
 	}
 }
